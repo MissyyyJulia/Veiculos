c35391d71425870011c1cdbb085d250cbe0ccd07
MissyyyJulia <julia.oliv4@outlook.com>
2025-06-08 18:55:49 -0300

    Criação das classes

--- veiculos/src/main/java/veiculos/Moto.java
+++ veiculos/src/main/java/veiculos/Moto.java
@@ -1,27 +1,5 @@
 package veiculos;
 
-//Herda atributos de Veiculo
 public class Moto extends Veiculo{
 
-	 private int cilindradas;
-
-	    public Moto(String placa, String modelo, String marca, int ano, int cilindradas) {
-	        super(placa, modelo, marca, ano);
-	        this.cilindradas = cilindradas;
-	    }
-
-	    public int getCilindradas() {
-	        return cilindradas;
-	    }
-
-	    public void setCilindradas(int cilindradas) {
-	        this.cilindradas = cilindradas;
-	    }
-
-	    @Override
-	    public void exibirDetalhes() {
-	        System.out.println("Moto -> " + getMarca() + " " + getModelo() + " (" + getAno() + "), Placa: " + getPlaca());
-	        System.out.println("Cilindradas: " + cilindradas + "cc");
-	    }
-	    
 }
--- veiculos/src/main/java/veiculos/Veiculo.java
+++ veiculos/src/main/java/veiculos/Veiculo.java
@@ -1,58 +1,5 @@
 package veiculos;
 
-//classe pai
 public class Veiculo {
-     String placa;
-     String modelo;
-     String marca;
-     int ano;
 
-    //construtor
-    public Veiculo(String placa, String modelo, String marca, int ano) {
-        this.placa = placa;
-        this.modelo = modelo;
-        this.marca = marca;
-        this.ano = ano;
-    }
-
-    //getters e setters (encapsulamento)
-    public String getPlaca() {
-		return placa;
-	}
-
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
-
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-
-	public int getAno() {
-		return ano;
-	}
-
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
-
-	//exibe detalhes do veículo
-    public void exibirDetalhes() {
-        System.out.println("Veículo - Modelo: " + modelo + ", Marca: " + marca + ", Placa: " + placa + ", Ano: " + ano);
-    }
-
-  
-    
 }
